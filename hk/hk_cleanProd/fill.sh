4468841ed156e7d17266e784dcc2e6a27703d6bd
sk1806 <soph.e.king123@gmail.com>
2019-04-02 06:00:59 +0100

    Fill local and dfc lists.  Will probably write this into python scripts, but will do for now

---
+++ hk/hk_cleanProd/fill.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirac-dms-find-lfns Path=/hyperk.org/beam/miniprod/A/1e22_HK_Tochibora/pos/wcsim/wcsim_dfc.list          > wcsim_dfc.list
+dirac-dms-find-lfns Path=/hyperk.org/beam/miniprod/A/1e22_HK_Tochibora/pos/fitqun/4_ring/fitun_dfc.list  > fitqun_dfc.list
+
