f86e8b0e270585787f8c95e266219c1c8ce07161
sk1806 <soph.e.king123@gmail.com>
2019-04-07 20:18:30 +0100

    Making sure up-to-date before deleting this local repo

--- libFns/dfc_getReplicaSize.py
+++ libFns/dfc_getReplicaSize.py
@@ -30,50 +30,62 @@ Script.parseCommandLine()
 from DIRAC.DataManagementSystem.Client.DataManager import DataManager
 dm = DataManager()
 
-
-#lfn = "/t2k.org/nd280/production006/L/mcp/neut/2015-08-water/magnet/run9/numc/oa_nt_beam_90910104-0038_e6emxphl6w3g_numc_000_magnet201508waterrun9.root"
-lfn='/t2k.org/nd280/production006/L/mcp/neut/2015-08-water/magnet/run9/numc/oa_nt_beam_90910059-0016_dm4kw3yo2aeb_numc_000_magnet201508waterrun9.root'
+#
+##lfn = "/t2k.org/nd280/production006/L/mcp/neut/2015-08-water/magnet/run9/numc/oa_nt_beam_90910104-0038_e6emxphl6w3g_numc_000_magnet201508waterrun9.root"
+#lfn='/t2k.org/nd280/production006/L/mcp/neut/2015-08-water/magnet/run9/numc/oa_nt_beam_90910059-0016_dm4kw3yo2aeb_numc_000_magnet201508waterrun9.root'
 site = "UKI-LT2-QMUL2-disk"
-
-res = dm.getReplicaSize(lfn, site)
-print('res = ' )
-print(res)
-
-size = res['Value']['Successful'][lfn]
-print('size = ')
-print(size)
-
+#
+#res = dm.getReplicaSize(lfn, site)
+#print('res = ' )
+#print(res)
+#
+#size = res['Value']['Successful'][lfn]
+#print('size = ')
+#print(size)
+#
 
 
 f_size = open("size.list", "w+")
 f_size_small = open("size_small.list", "w+")
 f_size_small_del = open("size_small_del.list", "w+")
+f_size_error = open("size_error.list", "w+")
 
-dfc_path = "/t2k.org/nd280/production006/L/mcp/neut/2015-08-water/magnet/run9/"
+#dfc_path = "/t2k.org/nd280/production006/L/mcp/neut/2015-08-water/magnet/run9/"
+#lower_lim = 30000000 
+#type_dir = "numc"
+#lower_lim = 170000000
+#type_dir = "anal"
+
+dfc_path = "/t2k.org/nd280/production006/T/mcp/anti-neut_D/2010-11-water/magnet/run5/"
+lower_lim = 19200000 
 type_dir = "numc"
+#lower_lim = 170000000
+#type_dir = "anal"
+
+
+
+
 dfc_list = dirac__ls(dfc_path + type_dir)
-lower_lim = 30000000 
 
 count=0
 
 for lfn in dfc_list:
     res = dm.getReplicaSize(lfn, site)
     print(lfn)
-    if not res:
-        print('ghhhhh')
-    size = res['Value']['Successful'][lfn]
-    str_lfn = lfn + '\n'
-    str_size_lfn = str(size) + " : " + str_lfn
-    print(str_size_lfn)
-    f_size.write(str_size_lfn)
-    if(size < lower_lim):
-        f_size_small.write(str_size_lfn)
-        f_size_small_del.write(str_lfn)
-
-#    count += 1
-#    if count ==30:
-#        break
-    #if
+    try:
+        size = res['Value']['Successful'][lfn]
+        str_lfn = lfn + '\n'
+        str_size_lfn = str(size) + " : " + str_lfn
+        print(str_size_lfn)
+        f_size.write(str_size_lfn)
+        if(size < lower_lim):
+            f_size_small.write(str_size_lfn)
+            f_size_small_del.write(str_lfn)
+    except:
+        print('Error for:  '),
+        print(lfn)
+        str_lhn = lfn + '\n'
+        f_size_error.write(lfn)
 
 
 f_size.close()
