cc63766c1fefbdd7621a6399237289592b7e37b7
sk1806 <soph.e.king123@gmail.com>
2019-04-13 16:18:53 +0100

    Tidying.  Should turn this into function at some point

--- libFns/dfc_getReplicaSize.py
+++ libFns/dfc_getReplicaSize.py
@@ -57,8 +57,10 @@ f_size_error = open("size_error.list", "w+")
 #type_dir = "anal"
 
 dfc_path = "/t2k.org/nd280/production006/T/mcp/anti-neut_D/2010-11-water/magnet/run5/"
+
 lower_lim = 19200000 
 type_dir = "numc"
+
 #lower_lim = 170000000
 #type_dir = "anal"
 
@@ -91,57 +93,5 @@ for lfn in dfc_list:
 f_size.close()
 f_size_small.close()      
 f_size_small_del.close()
-
-exit()
-
-######## Parts to Edit ########
-
-# local list of files (read-only)
-file_list = open('t5-t6.list', 'r')
-
-# local directory of files
-local_folder = '/data/king/t2k/GRID/dirac/dirac_api_tests'
-
-
-# LFNs that correspond to list of files (read-only)
-# Usuaully we use the file list to also deifne the LFNs
-# But you could provide a different list of corresponding LFNs if you wish
-lfn_list = open('t5-t6.list', 'r')
-
-# DFC directory of files
-# If the DFC folder does not exist, it will be created automatically
-dfc_folder = '/t2k.org/user/king/b'
-
-# Site where you wish to copy the files to
-site = 'UKI-LT2-QMUL2-disk'
-
-#############################
-
-print('debug 1')
-#print dm.putAndRegister( '/hyperk.org/user/king/t1.txt', '/data/king/t2k/GRID/dirac/dirac_api_tests/t1.txt' , site )
-print('debug 2')
-
-for i,j in  zip(file_list, lfn_list):
-
-    # use  rstrip() to remove the '\n' from reading the text file
-    local = local_folder + '/' + i.rstrip()  
-    dfc =   dfc_folder   + '/' + j.rstrip()
-    
-    print('')
-    print(local)
-    print(dfc)
-    print('')
-
-    print dm.putAndRegister( dfc, local, site )
-  
-
-file_list.close()
-lfn_list.close()
-
-
-
-
-
-
 
 
