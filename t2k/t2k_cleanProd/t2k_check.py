d0d819b2c74d447a25230ffb253170adb12ace84
sk1806 <soph.e.king123@gmail.com>
2019-04-22 00:15:22 +0100

    Hoping bug fixes due to multiple repeats of a file are now fixed

--- t2k/t2k_cleanProd/t2k_check.py
+++ t2k/t2k_cleanProd/t2k_check.py
@@ -150,11 +150,15 @@ def extract_miss_rep( numList, misList, repList , startRun, endRun, startSub=0, 
     if( int(num[1]) < startSub ):
         print('First file subrun number is below loop range!! ')
         exit()
-    
+
+    # i,j are the full set of numbers you are looking for
+    # num and lines_index searches the numbers you have in the file    
     for i in range (startRun, endRun, 1):
         for k in range (startSub, endSub, 1):
-            print(i, k)
-            print('checking lines[lines_index] = '+  lines[lines_index])
+            print('')
+            print('Searching:  '+ str(i) +' '+str(k) )
+            print('Checking line:      ' + str(num[0]).zfill(8) +' '+ str(num[1]).zfill(4) )
+            print('lines_index = ' + str(lines_index) )
 
             # check for repeated run codes     
             # check if line is the same as the one before
@@ -162,22 +166,24 @@ def extract_miss_rep( numList, misList, repList , startRun, endRun, startSub=0, 
             # then increase the line index (and hence 'num') 
             # so you can still do the other checks now you skipped to the next line
             # while keeping number i,k the same
-            if( lines_index>0 and lines[lines_index] == lines[lines_index -1] ):
+            while( lines_index>0 and lines[lines_index] == lines[lines_index -1]  ):
                 print('Repeated entry:  ' +  lines[lines_index] )
                 # write the line to the file (node the i,k because these are now ahead)
                 code = str(num[0]).zfill(8) +' '+ str(num[1]).zfill(4)
                 f_rep.write( code )
                 f_rep.write('\n')
                 #lines_index += 1
-                lines_index += 1            
                 # only need to update num when we increase the index
                 # provided it isnt at the end
-                if ( lines_index  != n_lines  ):
+                if ( lines_index    != n_lines - 1  ):
+                    lines_index += 1            
                     num = (  lines[lines_index].rstrip('\n')  ).split(' ', 1)  
-                 
+                    print(' - new index = ' + str(lines_index) )
+                    print(' - new line to check: ' + str(num[0]).zfill(8) +' '+ str(num[1]).zfill(4) )   
             # check if we exhausted list of files 
             # in which case just add to missing list
             if( lines_index  == n_lines  ):  
+                print('Exhausted list')
                 #print('End of file')
                 code = str(i).zfill(8) +' '+ str(k).zfill(4)
                 f_mis.write( code )
@@ -187,14 +193,18 @@ def extract_miss_rep( numList, misList, repList , startRun, endRun, startSub=0, 
                 continue
 
             if( (i,k) == ( int(num[0]), int(num[1]) ) ):
-                lines_index += 1            
+                print('Found ' + str(i) +' '+str(k) )
+                #lines_index += 1            
                 # only need to update num when we increase the index
                 # provided it isnt at the end
-                if ( lines_index  != n_lines  ):
+                if ( lines_index  != n_lines - 1  ):
+                    lines_index += 1            
                     num = (  lines[lines_index].rstrip('\n')  ).split(' ', 1)  
-                    #print('Found num = '),
-                    #print(num)
+                    print(' - new index = ' + str(lines_index) )
+                    print(' - new line to check: ' + str(num[0]).zfill(8) +' '+ str(num[1]).zfill(4) )   
+
             else:
+                print('Not found: ' + str(i) +' '+str(k) )
                 code = str(i).zfill(8) +' '+ str(k).zfill(4)
                 f_mis.write( code )
                 f_mis.write('\n')
